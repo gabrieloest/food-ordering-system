1fa8a6883fb8879bf9525d8a251af9658fe9c501
Gabriel Oest <gabriel.oest@etermax.com.uy>
2023-11-02 23:17:18 -0300

    Add ProductId value object

---
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/entity/valueobject/ProductId.java
@@ -0,0 +1,9 @@
+package com.food.ordering.system.domain.entity.valueobject;
+
+import java.util.UUID;
+
+public class ProductId extends BaseId<UUID> {
+    protected ProductId(UUID value) {
+        super(value);
+    }
+}
