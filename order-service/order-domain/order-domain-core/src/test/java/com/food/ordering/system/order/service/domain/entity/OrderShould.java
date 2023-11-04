f7b3b40d5ac25a643c4b2c20e4eaa92381079f67
Gabriel Oest <gabriel.oest@etermax.com.uy>
2023-11-03 22:40:31 -0300

    Add Order entity validateOrder method

--- order-service/order-domain/order-domain-core/src/test/java/com/food/ordering/system/order/service/domain/entity/OrderShould.java
+++ order-service/order-domain/order-domain-core/src/test/java/com/food/ordering/system/order/service/domain/entity/OrderShould.java
@@ -1,11 +1,17 @@
 package com.food.ordering.system.order.service.domain.entity;
 
+import com.food.ordering.system.domain.entity.valueobject.Money;
 import com.food.ordering.system.domain.entity.valueobject.OrderId;
 import com.food.ordering.system.domain.entity.valueobject.OrderStatus;
+import com.food.ordering.system.domain.entity.valueobject.ProductId;
+import com.food.ordering.system.order.service.domain.exception.OrderDomainException;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.List;
 import java.util.UUID;
 
@@ -14,36 +20,168 @@ import static org.mockito.Mockito.*;
 
 class OrderShould {
 
-    private UUID uuid;
+	private UUID uuid;
 
-    @BeforeEach
-    void setUp() {
-        uuid = mock(UUID.class);
-        mockStatic(UUID.class);
-        given(UUID.randomUUID()).willReturn(uuid);
-        given(uuid.toString()).willReturn(
-            "e246a687-661d-408c-9a70-72370bc439b8",
-            "662768d4-5f94-4833-b524-55edf721e9b8"
-        );
-    }
+	@BeforeEach
+	void setUp() {
+		uuid = mock(UUID.class);
+		mockStatic(UUID.class);
+		given(UUID.randomUUID()).willReturn(uuid);
+		given(uuid.toString()).willReturn(
+			"e246a687-661d-408c-9a70-72370bc439b8",
+			"662768d4-5f94-4833-b524-55edf721e9b8"
+		);
+	}
 
-    @org.junit.jupiter.api.Test
-    void initializeOrder() {
+	@AfterEach
+	void tearDown() {
+		clearAllCaches();
+	}
 
-        Order order = new Order(
-                Order.Builder.builder()
-                        .items(List.of(
-                                new OrderItem(OrderItem.Builder.builder()),
-                                new OrderItem(OrderItem.Builder.builder())
-                        ))
-        );
+	@Test
+	void initializeOrder() {
 
-        order.initializeOrder();
+		Order order = new Order(
+			Order.Builder.builder()
+				.items(List.of(
+					new OrderItem(OrderItem.Builder.builder()),
+					new OrderItem(OrderItem.Builder.builder())
+				))
+		);
 
-        Assertions.assertEquals(order.getId().getValue().toString(), "e246a687-661d-408c-9a70-72370bc439b8");
-        Assertions.assertEquals(order.getTrackingId().getValue().toString(), "662768d4-5f94-4833-b524-55edf721e9b8");
-        Assertions.assertEquals(order.getOrderStatus(), OrderStatus.PENDING);
-        Assertions.assertEquals(order.getItems().get(0).getId().getValue(), 1);
-        Assertions.assertEquals(order.getItems().get(1).getId().getValue(), 2);
-    }
+		order.initializeOrder();
+
+		Assertions.assertEquals(order.getId().getValue().toString(), "e246a687-661d-408c-9a70-72370bc439b8");
+		Assertions.assertEquals(order.getTrackingId().getValue().toString(), "662768d4-5f94-4833-b524-55edf721e9b8");
+		Assertions.assertEquals(order.getOrderStatus(), OrderStatus.PENDING);
+		Assertions.assertEquals(order.getItems().get(0).getId().getValue(), 1);
+		Assertions.assertEquals(order.getItems().get(1).getId().getValue(), 2);
+	}
+
+	@Test
+	void throwException_onValidateOrderValidateInitialOrder_withOrderId() {
+		Order order = new Order(
+			Order.Builder.builder()
+				.orderId(new OrderId(UUID.fromString("e246a687-661d-408c-9a70-72370bc439b8")))
+		);
+
+		Exception exception = Assertions.assertThrows(OrderDomainException.class, order::validateOrder);
+
+		Assertions.assertEquals("Order is not in correct state for initialization!", exception.getMessage());
+	}
+
+	@Test
+	void throwException_onValidateOrderValidateInitialOrder_withOrderStatus() {
+		Order order = new Order(
+			Order.Builder.builder()
+				.orderStatus(OrderStatus.PAID)
+		);
+
+		Exception exception = Assertions.assertThrows(OrderDomainException.class, order::validateOrder);
+
+		Assertions.assertEquals("Order is not in correct state for initialization!", exception.getMessage());
+	}
+
+	@Test
+	void throwException_onValidateOrderValidateTotalPrice_withNoPrice() {
+		Order order = new Order(
+			Order.Builder.builder().price(null)
+		);
+
+		Exception exception = Assertions.assertThrows(OrderDomainException.class, order::validateOrder);
+
+		Assertions.assertEquals("Total price must be greater than zero!", exception.getMessage());
+	}
+
+	@Test
+	void throwException_onValidateOrderValidateTotalPrice_withPriceZero() {
+		Order order = new Order(
+			Order.Builder.builder().price(new Money(BigDecimal.ZERO))
+		);
+
+		Exception exception = Assertions.assertThrows(OrderDomainException.class, order::validateOrder);
+
+		Assertions.assertEquals("Total price must be greater than zero!", exception.getMessage());
+	}
+
+	@Test
+	void throwException_onValidateOrderValidateItemsPrice_withWrongOrderPrice() {
+		Order order = new Order(
+			Order.Builder.builder()
+				.price(new Money(new BigDecimal(15)))
+				.items(List.of(
+					new OrderItem(OrderItem.Builder.builder()
+						.price(new Money(new BigDecimal(5)))
+						.quantity(2)
+						.subTotal(new Money(new BigDecimal(10).setScale(2, RoundingMode.HALF_EVEN)))
+						.product(new Product(new ProductId(UUID.randomUUID()), "Product 1", new Money(new BigDecimal(5))))),
+					new OrderItem(OrderItem.Builder.builder()
+						.price(new Money(new BigDecimal(5)))
+						.quantity(3)
+						.subTotal(new Money(new BigDecimal(15).setScale(2, RoundingMode.HALF_EVEN)))
+						.product(new Product(new ProductId(UUID.randomUUID()), "Product 2", new Money(new BigDecimal(5)))))
+				))
+		);
+
+		Exception exception = Assertions.assertThrows(OrderDomainException.class, order::validateOrder);
+		Assertions.assertEquals("Total price: 15 is not equal to Order items total: 25.00!",
+			exception.getMessage());
+	}
+
+	@Test
+	void throwException_onValidateOrderValidateItemsPrice_withOrderItemPriceDifferentFromProductPrice() {
+		Order order = new Order(
+			Order.Builder.builder()
+				.price(new Money(new BigDecimal(15)))
+				.items(List.of(
+					new OrderItem(OrderItem.Builder.builder()
+						.price(new Money(new BigDecimal(5)))
+						.quantity(2)
+						.subTotal(new Money(new BigDecimal(10).setScale(2, RoundingMode.HALF_EVEN)))
+						.product(new Product(new ProductId(UUID.randomUUID()), "Product 1", new Money(new BigDecimal(15)))))
+				))
+		);
+
+		Exception exception = Assertions.assertThrows(OrderDomainException.class, order::validateOrder);
+		Assertions.assertEquals("Order item price: 5 is not valid for product e246a687-661d-408c-9a70-72370bc439b8",
+			exception.getMessage());
+	}
+
+	@Test
+	void throwException_onValidateOrderValidateItemsPrice_withOrderItemPriceZero() {
+		Order order = new Order(
+			Order.Builder.builder()
+				.price(new Money(new BigDecimal(15)))
+				.items(List.of(
+					new OrderItem(OrderItem.Builder.builder()
+						.price(Money.ZERO)
+						.quantity(2)
+						.subTotal(new Money(new BigDecimal(10).setScale(2, RoundingMode.HALF_EVEN)))
+						.product(new Product(new ProductId(UUID.randomUUID()), "Product 1", new Money(new BigDecimal(15)))))
+				))
+		);
+
+		Exception exception = Assertions.assertThrows(OrderDomainException.class, order::validateOrder);
+		Assertions.assertEquals("Order item price: 0 is not valid for product e246a687-661d-408c-9a70-72370bc439b8",
+			exception.getMessage());
+	}
+
+	@Test
+	void throwException_onValidateOrderValidateItemsPrice_withOrderItemWrongSubtotal() {
+		Order order = new Order(
+			Order.Builder.builder()
+				.price(new Money(new BigDecimal(15)))
+				.items(List.of(
+					new OrderItem(OrderItem.Builder.builder()
+						.price(new Money(new BigDecimal(5)))
+						.quantity(2)
+						.subTotal(new Money(new BigDecimal(15).setScale(2, RoundingMode.HALF_EVEN)))
+						.product(new Product(new ProductId(UUID.randomUUID()), "Product 1", new Money(new BigDecimal(15)))))
+				))
+		);
+
+		Exception exception = Assertions.assertThrows(OrderDomainException.class, order::validateOrder);
+		Assertions.assertEquals("Order item price: 5 is not valid for product e246a687-661d-408c-9a70-72370bc439b8",
+			exception.getMessage());
+	}
 }
