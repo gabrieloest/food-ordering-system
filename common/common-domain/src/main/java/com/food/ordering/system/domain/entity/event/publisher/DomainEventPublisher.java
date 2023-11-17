5d9cd1469900c91ed489e74054e66187c45b2e13
Gabriel Oest <gabriel.oest@etermax.com.uy>
2023-11-16 23:29:29 -0300

    common-domain - Add DomainEventPublisher interface

---
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/entity/event/publisher/DomainEventPublisher.java
@@ -0,0 +1,8 @@
+package com.food.ordering.system.domain.entity.event.publisher;
+
+import com.food.ordering.system.domain.entity.event.DomainEvent;
+
+public interface DomainEventPublisher<T extends DomainEvent> {
+
+	void publish(T domainEvent);
+}
