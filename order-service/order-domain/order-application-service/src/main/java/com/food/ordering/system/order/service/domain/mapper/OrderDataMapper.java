f768295fec748048f69472c7dc823408d2cbacd8
Gabriel Oest <gabriel.oest@etermax.com.uy>
2023-11-17 00:09:17 -0300

    order-application-service - Add OrderApplicationServiceImpl WIP

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/mapper/OrderDataMapper.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/mapper/OrderDataMapper.java
@@ -1,7 +1,72 @@
 package com.food.ordering.system.order.service.domain.mapper;
 
+import com.food.ordering.system.domain.entity.valueobject.CustomerId;
+import com.food.ordering.system.domain.entity.valueobject.Money;
+import com.food.ordering.system.domain.entity.valueobject.ProductId;
+import com.food.ordering.system.domain.entity.valueobject.RestaurantId;
+import com.food.ordering.system.order.service.domain.dto.create.CreateOrderCommand;
+import com.food.ordering.system.order.service.domain.dto.create.CreateOrderResponse;
+import com.food.ordering.system.order.service.domain.dto.create.OderAddress;
+import com.food.ordering.system.order.service.domain.entity.Order;
+import com.food.ordering.system.order.service.domain.entity.OrderItem;
+import com.food.ordering.system.order.service.domain.entity.Product;
+import com.food.ordering.system.order.service.domain.entity.Restaurant;
+import com.food.ordering.system.order.service.domain.valueobject.StreetAddress;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Component
 public class OrderDataMapper {
+
+	public Restaurant createOrderCommandToRestaurant(CreateOrderCommand createOrderCommand){
+		return Restaurant.Builder.builder()
+			.restaurantId(new RestaurantId(createOrderCommand.getRestaurantId()))
+			.products(createOrderCommand.getItems().stream().map(orderItem ->
+				new Product(new ProductId(orderItem.getProductId())))
+				.collect(Collectors.toList()))
+			.build();
+	}
+
+	public Order createOrderCommandToOrder(CreateOrderCommand createOrderCommand){
+		return Order.Builder.builder()
+			.customerId(new CustomerId(createOrderCommand.getCustomerId()))
+			.restaurantId(new RestaurantId(createOrderCommand.getRestaurantId()))
+			.deliveryAddress(orderAddressToStreetAddress(createOrderCommand.getAddress()))
+			.price(new Money(createOrderCommand.getPrice()))
+			.items(orderItemsToOrderItemsEntities(createOrderCommand.getItems()))
+			.build();
+	}
+
+	public CreateOrderResponse orderToCreatedOrderResponse(Order order) {
+		return CreateOrderResponse.builder()
+			.orderTrackingId(order.getTrackingId().getValue())
+			.orderStatus(order.getOrderStatus())
+			.build();
+	}
+
+	private StreetAddress orderAddressToStreetAddress(OderAddress address) {
+		return new StreetAddress(
+			UUID.randomUUID(),
+			address.getStreet(),
+			address.getPostalCode(),
+			address.getCity()
+		);
+	}
+
+	private List<OrderItem> orderItemsToOrderItemsEntities(
+		@NotNull List<com.food.ordering.system.order.service.domain.dto.create.OrderItem> orderItems) {
+
+		return orderItems.stream().map(orderItem ->
+			OrderItem.Builder.builder()
+				.product(new Product(new ProductId(orderItem.getProductId())))
+				.price(new Money(orderItem.getPrice()))
+				.quantity(orderItem.getQuantity())
+				.subTotal(new Money(orderItem.getSubTotal()))
+				.build())
+			.collect(Collectors.toList());
+	}
 }
